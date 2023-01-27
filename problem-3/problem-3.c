66877fdf17476709095f74a9d82d6626279eea8a
florent-haxhiu <florent_h2@hotmail.com>
2023-01-27 10:03:52 +0000

    Completed problem three

---
+++ problem-3/problem-3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main() {
+  long findPrime = 600851475143;
+  long lastFactor, factor;
+
+  if (findPrime % 2 == 0) {
+    findPrime = findPrime / 2;
+    lastFactor = 2;
+    while (findPrime % 2 == 0) {
+      findPrime = findPrime / 2;
+    }
+  } else {
+    lastFactor = 1;
+  }
+  factor = 3;
+  while (findPrime > 1) {
+    if (findPrime % factor == 0) {
+      findPrime = findPrime / factor;
+      lastFactor = factor;
+      while (findPrime % factor == 0) {
+        findPrime = findPrime / factor;
+      }
+    }
+    factor = factor + 2;
+  }
+
+  printf("%ld\n", lastFactor);
+}
